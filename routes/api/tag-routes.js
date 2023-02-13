f63858da09385a9db19f4a032d98c3fc5a6722ab
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2023-02-13 15:50:45 -0800

    get route by id success

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag, as: "products" }],
     });
 
     if (!tagData) {
