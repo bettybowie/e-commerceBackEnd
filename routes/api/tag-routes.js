bfba415db4c0a51ef3bcf99d40b29830d8be00ef
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2023-02-14 18:13:16 -0800

    fixed put route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -45,7 +45,7 @@ router.post('/', (req, res) => {
         const productTagIdArr = req.body.productIds.map((product_id) => {
           return {
             product_id,
-            tag_id: tag_id,
+            tag_id: tag.id,
           };
         });
         return ProductTag.bulkCreate(productTagIdArr);
@@ -62,41 +62,22 @@ router.post('/', (req, res) => {
 });
 
 // PUT route to update a tag's name by its id 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
 
   Tag.update(req.body, {
     where: {
       id: req.params.id,
     },
   })
-    .then((tag) => {
-      return ProductTag.findAll({ where: { tag_id: req.params.id } });
+    .then((tagData) => {
+      if (!tagData) {
+        res.status(404).json({ message: 'No category found with that id!'});
+        return;
+      };
+      res.status(200).json(tagData); 
     })
-    .then((productTags) => {
-      const productTagIds = productTags.map(({ product_id }) => product_id);
-
-      const newProductTags = req.body.productTagIds
-        .filter((product_id) => !productTagIds.includes(product_id))
-        .map((product_id) => {
-          return {
-            product_id,
-            tag_id: req.params.id,
-          };
-        });
-
-        const productTagsToRemove = productTags
-          .filter(({ product_id }) => !req.body.productTagIds.includes(product_id))
-          .map(({ id }) => id);
-
-        return Promise.all([
-          ProductTag.destroy({ where: { id: productTagsToRemove } }),
-          ProductTag.bulkCreate(newProductTags),
-        ]);
-    })
-    .then((updatedProductTags) => res.json(updatedProductTags))
-    .catch((err) => {
-      res.status(400).json(err);
-    })
+    .catch((err) => res.status(400).json(err));
+ 
 });
 
 // DELETE route to delete one tag by its id value
